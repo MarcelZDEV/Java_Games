b54475ec6fb152db90884578f5e560df378ce2c9
MarcelZDEV <marcelowatyz@gmail.com>
2022-01-24 11:53:40 +0100

    ProgramToExpenses

--- src/com/company/ProgramToExpenses.java
+++ src/com/company/ProgramToExpenses.java
@@ -12,31 +12,36 @@ public class ProgramToExpenses {
             e.printStackTrace();
         }
     }
+
     public static void main(String[] args) {
         Scanner Scan = new Scanner(System.in);
         HashMap<String, Integer> YourExpenses = new HashMap<String, Integer>();
         int TotalExpenses = 0;
-        System.out.println("+++++++++++Welcome in Program to your Expense's+++++++++++");
-        System.out.println("+++++++++++1 Menu+++++++++++");
-        System.out.println("+++++++++++2 Quit+++++++++++");
+        System.out.println("-----------Welcome in Program to your Expense's-----------");
+        System.out.println("-----------1 Menu-----------");
+        System.out.println("-----------2 Quit-----------");
         String MenuUser = Scan.nextLine();
         while (true) {
             if (MenuUser.equals("1")) {
-                System.out.println("+++++++++++Menu+++++++++++");
-                System.out.println("+++++++++++1 Add Expenses+++++++++++");
-                System.out.println("+++++++++++2 Show Expenses+++++++++++");
-                System.out.println("+++++++++++3 Clear All Expenses+++++++++++");
-                System.out.println("+++++++++++4 Delete Expenses+++++++++++");
-                System.out.println("+++++++++++5 Quit+++++++++++");
+                System.out.println("-----------Menu-----------");
+                System.out.println("-----------1 Add Expenses-----------");
+                System.out.println("-----------2 Show Expenses-----------");
+                System.out.println("-----------3 Clear All Expenses-----------");
+                System.out.println("-----------4 Delete Expenses-----------");
+                System.out.println("-----------5 Quit-----------");
                 String MenuUserExpenses = Scan.nextLine();
                 if (MenuUserExpenses.equals("1")) {
                     System.out.println("Type here your expense name");
                     String ExpenseAddName = Scan.nextLine();
                     System.out.println("Type here your expense");
-                    Integer ExpenseAddCost = Integer.parseInt(Scan.nextLine());
-                    YourExpenses.put(ExpenseAddName, ExpenseAddCost);
-                    System.out.println("Your expense is added");
-                    TotalExpenses = TotalExpenses + ExpenseAddCost;
+                    int ExpenseAddCost = Integer.parseInt(Scan.nextLine());
+                    if (ExpenseAddCost % 1 == 0) {
+                        YourExpenses.put(ExpenseAddName, ExpenseAddCost);
+                        System.out.println("Your expense is added");
+                        TotalExpenses = TotalExpenses + ExpenseAddCost;
+                    }else {
+                        System.out.println("Value must have only numbers");
+                    }
                     Scan.nextLine();
                 } else if (MenuUserExpenses.equals("2")) {
                     System.out.println("Your total expenses: " + TotalExpenses);
